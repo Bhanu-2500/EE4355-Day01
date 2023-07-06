9cf3ecce2eb6ab5e7837339edcd1afd988f18280
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-07-06 20:03:39 +0530

    Write simple codes in IDE in Java

---
+++ Day-01/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter userName:" );
+        String userName = myObj.nextLine();
+        System.out.println("User name is: "+ userName);
+
+    }
+}
