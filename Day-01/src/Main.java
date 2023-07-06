f3d820997994db34b852ead95b85c1cf11456f0e
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-07-07 04:38:39 +0530

    familiarise with variable's type and casting

--- Day-01/src/Main.java
+++ Day-01/src/Main.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        char symbol = '\u0040';
-        System.out.println(symbol);
+        int math =0;
+        int science =0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the marks for the science");
+        science = scanner.nextInt();
+        System.out.println("Enter the marks for the math");
+        math = scanner.nextInt();
+        double average = (double)(science+math)/2;
+        System.out.println("Average marks: "+average);
 
     }
 }
